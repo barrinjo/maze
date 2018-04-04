33cb9ddfe59d1777d2afef67a1a03b10561c6c57
Joshua Barringer <jbarringer1999@gmail.com>
2018-04-04 16:00:45 -0700

    changed to vector coordinate printing system

--- mazePrint.cpp
+++ mazePrint.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 int main () {
 	string line;
+	//int y = 0;
+	vector<string> lines;
 
 	ifstream file ("floorplan");
 	if (file.is_open()) {
+
 		while(getline(file, line)) {
-			for (int i = 0; i <= 30; i++) {
-				if (line[i] == 'W') {
+			lines.push_back (line);
+		}
+		file.close();
+
+		for (int y = 0; y < lines.size(); ++y)
+		{
+			for (int x = 0; x < lines[y].length(); ++x)
+			{
+				if (lines[y][x] == 'W') {
 					cout << "█";
 				}
-				else if (line[i] == 'S') {
+				else if (lines[y][x] == 'S') {
 					cout << "☺";
 				}
-				else if (line[i] == 'F') {
+				else if (lines[y][x] == 'F') {
 					cout << " ";
 				}
 				else {
-					cout << line[i];
+					cout << lines[y][x];
 				}
 			}
 			cout << endl;
 		}
+
 	}
+
 	else {
 	cout << "floorplan not found\n";
 	}
