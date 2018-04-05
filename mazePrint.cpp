4e20292e2840da7608a707bcaf38a3c2069459b2
Joshua Barringer <jbarringer1999@gmail.com>
2018-04-04 21:05:35 -0700

    finished moving through the maze

--- mazePrint.cpp
+++ mazePrint.cpp
@@ -1,22 +1,85 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unistd.h>
+#include <termios.h>
 using namespace std;
 
-int main () {
-	string line;
-	//int y = 0;
-	vector<string> lines;
+string line;
+string input;
+//int y = 0;
+vector<string> lines;
+int coordX, coordY;
 
+void printMaze();
+char getch();
+
+int main () {
 	ifstream file ("floorplan");
 	if (file.is_open()) {
 
 		while(getline(file, line)) {
 			lines.push_back (line);
 		}
+		printMaze();
 		file.close();
 
-		for (int y = 0; y < lines.size(); ++y)
+	}
+
+	else {
+	cout << "floorplan not found\n";
+	}
+	
+	for (;;) {
+		cout << "move with wasd" << endl;
+		input = getch();
+		if (input == "w") { 
+			if (lines[coordY - 1][coordX] == 'F') {
+				cout << "YOU WIN!!!" << endl;
+				return 0;
+			}
+			if (lines[coordY - 1][coordX] != 'W') {
+				lines[coordY][coordX] = ' ';
+				lines[coordY - 1][coordX] = 'S';
+			}
+		}
+		if (input == "a") { 
+			if (lines[coordY][coordX - 1] == 'F') {
+				cout << "YOU WIN!!!" << endl;
+				return 0;
+			}
+			if (lines[coordY][coordX - 1] != 'W') {
+				lines[coordY][coordX] = ' ';
+				lines[coordY][coordX - 1] = 'S';
+			}
+		}
+		if (input == "s") { 
+			if (lines[coordY + 1][coordX] == 'F') {
+				cout << "YOU WIN!!!" << endl;
+				return 0;
+			}
+			if (lines[coordY + 1][coordX] != 'W') {
+				lines[coordY][coordX] = ' ';
+				lines[coordY + 1][coordX] = 'S';
+			}
+		}
+		if (input == "d") { 
+			if (lines[coordY][coordX + 1] == 'F') {
+				cout << "YOU WIN!!!" << endl;
+				return 0;
+			}
+			if (lines[coordY][coordX + 1] != 'W') {
+				lines[coordY][coordX] = ' ';
+				lines[coordY][coordX + 1] = 'S';
+			}
+		}
+		printMaze();
+	}
+}
+
+void printMaze() {
+	cout << "\033[2J\033[1;1H";
+	for (int y = 0; y < lines.size(); ++y) {
 		{
 			for (int x = 0; x < lines[y].length(); ++x)
 			{
@@ -25,9 +88,11 @@ int main () {
 				}
 				else if (lines[y][x] == 'S') {
 					cout << "☺";
+					coordY = y;
+					coordX = x;
 				}
 				else if (lines[y][x] == 'F') {
-					cout << " ";
+					cout << "->";
 				}
 				else {
 					cout << lines[y][x];
@@ -35,10 +100,25 @@ int main () {
 			}
 			cout << endl;
 		}
-
 	}
+}
 
-	else {
-	cout << "floorplan not found\n";
-	}
+char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
 }
